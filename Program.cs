2bc20fc9030274b6d7a68069203a968d0ca19dbc
nayuza <128115078+nayuza@users.noreply.github.com>
2023-03-27 11:23:01 +0700

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaxiApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // membuat object taxi
+            Taxi taxi = new Taxi();
+
+            //seting nilai properties
+            taxi.DriverName = "Rifqi";
+            taxi.OnDuty = true;
+            taxi.NumPassenger = 10;
+
+            //pemanggilan method
+            taxi.TaxiInfo();
+            taxi.PickUpPassenger();
+            taxi.DropOffPassenger();
+
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ Taxi.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaxiApp
+{
+    class Taxi
+    {
+        //properties
+        public string DriverName { get; set; }
+        public bool OnDuty { get; set; }
+        public int NumPassenger { get; set; }
+
+        //method
+        public void TaxiInfo()
+        {
+            Console.WriteLine("Driver Name : {0}", DriverName);
+            Console.WriteLine("On Duty : {0}", OnDuty);
+            Console.WriteLine("Number of Passenger: {0}", NumPassenger);
+            
+        }
+
+        public void PickUpPassenger()
+        {
+            Console.WriteLine("{0} sedang menjemput penumpang", DriverName);
+ 
+            Console.WriteLine("================================================");
+        }
+
+        public void DropOffPassenger()
+        {
+
+            Console.WriteLine("{0} telah selesai mengantar penumpang", DriverName);
+            Console.WriteLine("================================================");
+        }
+    }
+}
